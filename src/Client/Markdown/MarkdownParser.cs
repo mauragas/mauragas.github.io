5acf6dc670ec1ddf31492c8567cee1219bb9cc9a
Karolis Mauragas <karolism8@gmail.com>
2020-02-22 20:54:10 +0100

    - Added logo - Updated navigation - Removed article folder - Added redirection to download article files from different branch

--- src/Client/Markdown/MarkdownParser.cs
+++ src/Client/Markdown/MarkdownParser.cs
@@ -1,60 +1,61 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Markdig;
 using Markdig.Extensions.AutoIdentifiers;
-using System.Threading.Tasks;
-using System.Net.Http;
 
 namespace Application.Markdown
 {
     public class MarkdownParser
     {
+        private static readonly Uri BaseUri = new Uri("https://raw.githubusercontent.com/mauragas/Mauragas.github.io/articles");
+        private static readonly HttpClient _httpCLient = new HttpClient();
         private readonly MarkdownPipeline _markdownPipeline;
-        private readonly HttpClient _httpCLient;
 
         public MarkdownParser(HttpClient httpClient)
         {
             _markdownPipeline = GetMarkdownPipeline();
-            _httpCLient = httpClient;
         }
 
-        public async Task<string> ParseToHtmlAsync(string path)
+        public async Task<string> ParseToHtmlAsync(string relativeUri)
         {
-            var documentString = await _httpCLient.GetStringAsync(path);
+            var documentString = await _httpCLient.GetStringAsync(BaseUri + relativeUri);
             return Markdig.Markdown.ToHtml(documentString, _markdownPipeline);
         }
 
         private MarkdownPipeline GetMarkdownPipeline()
         {
             return new MarkdownPipelineBuilder()
-                        .UseAbbreviations()
-                        .UseAdvancedExtensions()
-                        .UseAutoIdentifiers(AutoIdentifierOptions.GitHub)
-                        .UseAutoLinks()
-                        .UseBootstrap()
-                        .UseCitations()
-                        .UseCustomContainers()
-                        .UseDefinitionLists()
-                        .UseDiagrams()
-                        .UseEmojiAndSmiley(true)
-                        .UseEmphasisExtras()
-                        .UseFigures()
-                        .UseFooters()
-                        .UseFootnotes()
-                        .UseGenericAttributes()
-                        .UseGlobalization()
-                        .UseGridTables()
-                        .UseListExtras()
-                        .UseMathematics()
-                        .UseMediaLinks()
-                        .UseNoFollowLinks()
-                        .UsePipeTables()
-                        .UsePragmaLines()
-                        .UsePreciseSourceLocation()
-                        .UseSmartyPants()
-                        .UseSoftlineBreakAsHardlineBreak()
-                        .UseTaskLists()
-                        .UseYamlFrontMatter()
-                        //.UseSyntaxHighlighting() // TODO Install extension when supported https://github.com/RichardSlater/Markdig.SyntaxHighlighting
-                        .Build();
+                .UseAbbreviations()
+                .UseAdvancedExtensions()
+                .UseAutoIdentifiers(AutoIdentifierOptions.GitHub)
+                .UseAutoLinks()
+                .UseBootstrap()
+                .UseCitations()
+                .UseCustomContainers()
+                .UseDefinitionLists()
+                .UseDiagrams()
+                .UseEmojiAndSmiley(true)
+                .UseEmphasisExtras()
+                .UseFigures()
+                .UseFooters()
+                .UseFootnotes()
+                .UseGenericAttributes()
+                .UseGlobalization()
+                .UseGridTables()
+                .UseListExtras()
+                .UseMathematics()
+                .UseMediaLinks()
+                .UseNoFollowLinks()
+                .UsePipeTables()
+                .UsePragmaLines()
+                .UsePreciseSourceLocation()
+                .UseSmartyPants()
+                .UseSoftlineBreakAsHardlineBreak()
+                .UseTaskLists()
+                .UseYamlFrontMatter()
+                //.UseSyntaxHighlighting() // TODO Install extension when supported https://github.com/RichardSlater/Markdig.SyntaxHighlighting
+                .Build();
         }
     }
 }
